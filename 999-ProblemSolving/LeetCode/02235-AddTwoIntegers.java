ac2262ef613de666af94563575ae85c5248dc6a3
Arnold Joel Joshua <marnoldjoeljoshua@gmail.com>
2026-09-16 18:39:53 +0530

    Create 02235-AddTwoIntegers.java

---
+++ 999-ProblemSolving/LeetCode/02235-AddTwoIntegers.java
@@ -0,0 +1,14 @@
+/*
+ * LeetCode #2235 - Add Two Integers
+ * Solved on: January 06, 2026
+ *
+ * Approach: Direct Addition
+ * Time Complexity: O(1)
+ * Space Complexity: O(1)
+ */
+
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
