20ed9c90f6b38dc3dcd9da24e369fef18f1a8ab1
Arnold Joel Joshua <marnoldjoeljoshua@gmail.com>
2026-09-22 12:01:12 +0530

    Update 00189-RotateArray.java

--- 999-ProblemSolving/LeetCode/00189-RotateArray.java
+++ 999-ProblemSolving/LeetCode/00189-RotateArray.java
@@ -1,12 +1,39 @@
 /*
  * LeetCode #189 - Rotate Array
- * Solved on: September 22, 2026
  *
- * Approach: Auxiliary Array
+ * Approach 1: Array Reversal
+ * Solved on: September 22, 2026
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ *
+ * Approach 2: Auxiliary Array
+ * Solved on: September 22, 2026
  * Time Complexity: O(n)
  * Space Complexity: O(n)
  */
 
+// Approach 1
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length);
+        reverse(nums, 0, k);
+        reverse(nums, k, nums.length);
+    }
+
+    static void reverse(int[] arr, int low, int high){
+        high--;
+        while(low < high){
+            int temp = arr[low];
+            arr[low] = arr[high];
+            arr[high] = temp;
+            low++;
+            high--;
+        }
+    }
+}
+
+// Approach 2
 class Solution {
     public void rotate(int[] nums, int k) {
         int length = nums.length;
